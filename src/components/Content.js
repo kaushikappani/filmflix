0a170ced7dce595a56e17dd6f7b5d3f1b4de11a2
info <58048398+kaushikappani@users.noreply.github.com>
2021-06-05 15:44:20 +0530

    UI changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,11 +7,12 @@ import ContentModel from "./ContentModel"
 const Content = ({ id, poster, title, date, media, rating }) => {
     return (
         <ContentModel media={media} id={id}>
-            <img className='poster' src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
-          <b className='title'>{title}</b>
-                <div className='subTitle'>{media === 'tv' ? "TV Series" : "Movie"}</div>
-                <div className='subTitle'>{date}</div>
-                {rating > 0 && <Rating name="half-rating-read" defaultValue={rating / 2} precision={0.1} readOnly />}
+            {/* <div className="content_img"><img className='poster' src={poster ? `${img_300}/${poster}` : unavailable} alt={title} /></div> */}
+            <div className="img_div"><img className='poster' src={poster ? `${img_300}/${poster}` : unavailable} alt={title} /></div>
+            <b className='title'>{title}</b>
+            <div className='subTitle'>{media === 'tv' ? "TV Series" : "Movie"}</div>
+            <div className='subTitle'>{date}</div>
+            { rating > 0 && <Rating name="half-rating-read" defaultValue={rating / 2} precision={0.1} readOnly />}
         </ContentModel>
     )
 }
